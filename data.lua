4375551b369cf48395e75b7857f4944c55a18a47
kingarthur91 <arthurkoy_durbin@yahoo.com>
2024-10-26 19:57:07 -0400

    first time uploading this to github

---
+++ data.lua
@@ -0,0 +1,5 @@
+-- Make minimal changes to the radar prototype
+data.raw.radar["radar"].max_distance_of_sector_revealed = 28
+data.raw.radar["radar"].max_distance_of_nearby_sector_revealed = 6
+data.raw.radar["radar"].energy_per_nearby_scan = "250kJ"
+data.raw.radar["radar"].energy_usage = "1200kW"
